c0a5649c22d40a1b4bb115d49be021623c53fae0
lijianfei <lijianfei@169.254.225.240>
2019-04-08 10:14:53 +0800

    添加数据格式转换类

--- jeffrey-backend/src/main/java/com/learn/jeffrey/controller/TestController.java
+++ jeffrey-backend/src/main/java/com/learn/jeffrey/controller/TestController.java
@@ -1,20 +1,16 @@
 package com.learn.jeffrey.controller;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.dom4j.Document;
 import org.dom4j.DocumentException;
-import org.dom4j.Element;
-import org.dom4j.io.SAXReader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.learn.jeffrey.utils.DataFormatUtil;
 
 /**
  * @author Jeffrey
@@ -26,31 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController{
     
     @PostMapping("/h")
-    public String hello(){
-        return "heelo";
+    public Map<String , String> hello(HttpServletRequest request) throws IOException, DocumentException{
+        return DataFormatUtil.xmlToMap(request);
     }
-    
-    /**
-     * 将接收到的XML转为map
-     * @param request
-     * @return
-     * @throws IOException
-     * @throws DocumentException
-     */
-    public Map<String , String> xmlToMap(HttpServletRequest request) throws IOException, DocumentException {
-        
-        Map<String , String> map=new  HashMap<String, String>();
-        SAXReader reader=new  SAXReader();
-        
-        InputStream is=request.getInputStream();
-        Document doc=reader.read(is);
-        Element root=doc.getRootElement();
-        List<Element> list=root.elements();
-        for(Element e:list) {
-            map.put(e.getName(), e.getText());
-        }
-        is.close();
-        return  map;      
-   }
     
 }
---
+++ jeffrey-backend/src/main/java/com/learn/jeffrey/utils/DataFormatUtil.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.learn.jeffrey.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+/**
+ * <p>description</p>
+ * @author lijianfei
+ * @date 2019-04-08
+ */
+public class DataFormatUtil {
+	
+	 /**
+     * 将接收到的XML转为map
+     * @param request
+     * @return
+     * @throws IOException
+     * @throws DocumentException
+     */
+    public static Map<String , String> xmlToMap(HttpServletRequest request) throws IOException, DocumentException {
+        
+        Map<String , String> map=new  HashMap<String, String>();
+        SAXReader reader=new  SAXReader();
+        
+        InputStream is=request.getInputStream();
+        Document doc=reader.read(is);
+        Element root=doc.getRootElement();
+        List<Element> list=root.elements();
+        for(Element e:list) {
+            map.put(e.getName(), e.getText());
+        }
+        is.close();
+        return  map;      
+   }
+
+}
