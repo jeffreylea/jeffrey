ec6ba3a97c20d24a4947cb79effb1ef6d175ee92
lijianfei <lijianfei@169.254.225.240>
2019-04-08 11:37:02 +0800

    添加map转json方法

--- jeffrey-backend/src/main/java/com/learn/jeffrey/controller/TestController.java
+++ jeffrey-backend/src/main/java/com/learn/jeffrey/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.learn.jeffrey.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSONObject;
 import com.learn.jeffrey.utils.DataFormatUtil;
 
 /**
@@ -25,5 +27,16 @@ public class TestController{
     public Map<String , String> hello(HttpServletRequest request) throws IOException, DocumentException{
         return DataFormatUtil.xmlToMap(request);
     }
-    
+
+   public static void main(String[] args) {
+	Map<Object, Object> map=new HashMap<>();
+	map.put("key1", "value1");
+	map.put("key2", 2);
+	map.put(3, 3);
+	
+	Map<Object, Object> map1=new HashMap<>();
+	map1.put("map1", map);
+	System.out.println(DataFormatUtil.mapToJson(map1));;
+	
+}
 }
--- jeffrey-backend/src/main/java/com/learn/jeffrey/utils/DataFormatUtil.java
+++ jeffrey-backend/src/main/java/com/learn/jeffrey/utils/DataFormatUtil.java
@@ -16,34 +16,51 @@ import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
+import com.alibaba.fastjson.JSONObject;
+
 /**
- * <p>description</p>
+ * <p>
+ * description
+ * </p>
+ * 
  * @author lijianfei
  * @date 2019-04-08
  */
 public class DataFormatUtil {
+
+	/**
+	 * dependency: dom4j 
+	 * 将接收到的XML转为map
+	 * @param request
+	 * @return
+	 * @throws IOException
+	 * @throws DocumentException
+	 */
+	@SuppressWarnings("unchecked")
+	public static Map<String, String> xmlToMap(HttpServletRequest request) throws IOException, DocumentException {
+
+		Map<String, String> map = new HashMap<String, String>();
+		SAXReader reader = new SAXReader();
+
+		InputStream is = request.getInputStream();
+		Document doc = reader.read(is);
+		Element root = doc.getRootElement();
+		List<Element> list = root.elements();
+		for (Element e : list) {
+			map.put(e.getName(), e.getText());
+		}
+		is.close();
+		return map;
+	}
 	
-	 /**
-     * 将接收到的XML转为map
-     * @param request
-     * @return
-     * @throws IOException
-     * @throws DocumentException
-     */
-    public static Map<String , String> xmlToMap(HttpServletRequest request) throws IOException, DocumentException {
-        
-        Map<String , String> map=new  HashMap<String, String>();
-        SAXReader reader=new  SAXReader();
-        
-        InputStream is=request.getInputStream();
-        Document doc=reader.read(is);
-        Element root=doc.getRootElement();
-        List<Element> list=root.elements();
-        for(Element e:list) {
-            map.put(e.getName(), e.getText());
-        }
-        is.close();
-        return  map;      
-   }
+	/**
+	 * 将map转化为json
+	 * dependency: fastjson 
+	 * @param map
+	 * @return
+	 */
+	public static Object mapToJson(Map<Object, Object> map) {
+		return JSONObject.toJSON(map);
+	}
 
 }
