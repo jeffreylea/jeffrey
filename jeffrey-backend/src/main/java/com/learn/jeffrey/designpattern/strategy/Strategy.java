a3df9764bb7a4b6d8c003e1991fb21968b66e936
JeffreyLea <1020724110@qq.com>
2019-12-03 09:10:05 +0800

    20191202

---
+++ jeffrey-backend/src/main/java/com/learn/jeffrey/designpattern/strategy/Strategy.java
@@ -0,0 +1,10 @@
+package com.learn.jeffrey.designpattern.strategy;
+
+/**
+ * <p>description</p>
+ * @author lijianfei
+ * @date 2019-12-02
+ */
+public interface Strategy {
+	public int doOperation(int num1,int num2);
+}
