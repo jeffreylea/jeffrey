d67beb63329add1daa297a5fac5051f54afd3a99
lijianfei <lijianfei@inspur.com>
2020-03-20 09:05:18 +0800

    1

--- jeffrey-learn-springboot/src/main/java/com/jeffrey/weibo/controller/WeiboController.java
+++ jeffrey-learn-springboot/src/main/java/com/jeffrey/weibo/controller/WeiboController.java
@@ -1,10 +1,22 @@
 package com.jeffrey.weibo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import weibo4j.Oauth;
+import weibo4j.http.AccessToken;
+import weibo4j.model.WeiboException;
+
 /**
  * @author lijianfei
  *<p></p>
  * 2020年3月19日
  */
 public class WeiboController {
+	Oauth oauth = new Oauth();
 
+	@RequestMapping("getAccessTokenByCode")
+	public AccessToken getAccessTokenByCode(String code) throws WeiboException {
+		return oauth.getAccessTokenByCode(code);
+	}
 }
